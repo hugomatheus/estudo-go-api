200999a8ad16c4965b061c9436dd73f202be073a
Hugo <hg.targino@gmail.com>
2024-03-30 09:19:05 -0300

    feat: endpoint crud product handlers

--- internal/dto/dto.go
+++ internal/dto/dto.go
@@ -1,6 +1,14 @@
 package dto
 
+import "github.com/hugomatheus/go-api/pkg/entity"
+
 type CreateProductInput struct {
 	Name  string  `json:"name"`
 	Price float64 `json:"price"`
+}
+
+type UpdateProductInput struct {
+	ID    entity.ID `json:"id"`
+	Name  string    `json:"name"`
+	Price float64   `json:"price"`
 }
--- internal/infra/database/product_db.go
+++ internal/infra/database/product_db.go
@@ -23,19 +23,39 @@ func (p *Product) FindByID(id string) (*entity.Product, error) {
 	return &product, err
 }
 
-func (p *Product) FindAll(page, limit int, sort string) ([]entity.Product, error) {
+type FindAllProductResponse struct {
+	Products []entity.Product `json:"products"`
+	Total    int64            `json:"total"`
+}
+
+func (p *Product) FindAll(page, limit int, sort string) (FindAllProductResponse, error) {
 	var products []entity.Product
 	var err error
+	var total int64
+
+	err = p.DB.Model(&entity.Product{}).Count(&total).Error
+	if err != nil {
+		return FindAllProductResponse{}, err
+	}
 
 	if sort != "" && sort != "asc" && sort != "desc" {
 		sort = "asc"
 	}
+
 	if page != 0 && limit != 0 {
 		err = p.DB.Limit(limit).Offset((page - 1) * limit).Order("created_at" + " " + sort).Find(&products).Error
 	} else {
 		err = p.DB.Order("created_at" + sort).Find(&products).Error
 	}
-	return products, err
+
+	if err != nil {
+		return FindAllProductResponse{}, err
+	}
+
+	return FindAllProductResponse{
+		Products: products,
+		Total:    total,
+	}, err
 }
 
 func (p *Product) Update(product *entity.Product) error {
