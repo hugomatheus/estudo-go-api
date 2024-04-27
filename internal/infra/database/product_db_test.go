734e0f0eba85c71280d57122870e4a3ca04de5ef
Hugo <hg.targino@gmail.com>
2024-04-27 12:21:28 -0300

    wip: tentativa de cobrir error de product_db

--- internal/infra/database/product_db_test.go
+++ internal/infra/database/product_db_test.go
@@ -1,13 +1,55 @@
 package database
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 	"testing"
 
 	"github.com/hugomatheus/go-api/internal/entity"
 	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
+
+type MockProductDB struct {
+	ProductDB *gorm.DB
+}
+
+func NewMockProductDB(db *gorm.DB) *MockProductDB {
+	return &MockProductDB{ProductDB: db}
+}
+
+func (m *MockProductDB) FindByID(id string) (*entity.Product, error) {
+	return nil, errors.New("produto não foi encontrado")
+}
+
+func (m *MockProductDB) Delete(id string) error {
+	return errors.New("produto não foi deletado")
+}
+
+func (m *MockProductDB) Create(product *entity.Product) error {
+	return errors.New("produto não foi criado")
+}
+
+func TestDeleteProductWithError(t *testing.T) {
+	SetupDBTest(&entity.Product{})
+	defer CloseDBTest()
+
+	product, err := entity.NewProduct("Product 1", 1030.9)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, product.ID)
+
+	productDB := NewProduct(GetDBTest())
+	err = productDB.Create(product)
+	assert.Nil(t, err)
+
+	mockProductDB := NewMockProductDB(GetDBTest())
+
+	err = mockProductDB.Delete(product.ID.String())
+	assert.NotNil(t, err)
+	assert.NotEmpty(t, err)
+	assert.Error(t, err, "produto não foi deletado")
+}
 
 func TestCreateProduct(t *testing.T) {
 	SetupDBTest(&entity.Product{})
@@ -21,6 +63,24 @@ func TestCreateProduct(t *testing.T) {
 	err = productDB.Create(product)
 	assert.Nil(t, err)
 
+}
+
+func TestFindByIDError(t *testing.T) {
+	SetupDBTest(&entity.Product{})
+	defer CloseDBTest()
+
+	product, err := entity.NewProduct("Produto 1", 10.4)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, product.ID)
+
+	productDB := NewProduct(GetDBTest())
+	err = productDB.Create(product)
+	assert.Nil(t, err)
+
+	mockProductDB := NewMockProductDB(GetDBTest())
+	_, err = mockProductDB.FindByID("2333")
+	assert.NotNil(t, err)
+	assert.NotEmpty(t, err)
 }
 
 func TestFindByID(t *testing.T) {
@@ -56,38 +116,38 @@ func TestFindAll(t *testing.T) {
 	products, err := productDB.FindAll(1, 10, "asc")
 	assert.Nil(t, err)
 	assert.NotEmpty(t, products)
-	assert.Len(t, products, 10)
-	assert.Equal(t, "Product 1", products[0].Name)
-	assert.Equal(t, "Product 10", products[9].Name)
+	assert.Len(t, products.Products, 10)
+	assert.Equal(t, "Product 1", products.Products[0].Name)
+	assert.Equal(t, "Product 10", products.Products[9].Name)
 
 	products, err = productDB.FindAll(2, 10, "asc")
 	assert.Nil(t, err)
 	assert.NotEmpty(t, products)
-	assert.Len(t, products, 10)
-	assert.Equal(t, "Product 11", products[0].Name)
-	assert.Equal(t, "Product 20", products[9].Name)
+	assert.Len(t, products.Products, 10)
+	assert.Equal(t, "Product 11", products.Products[0].Name)
+	assert.Equal(t, "Product 20", products.Products[9].Name)
 
 	products, err = productDB.FindAll(3, 10, "asc")
 	assert.Nil(t, err)
-	assert.Len(t, products, 5)
-	assert.Equal(t, "Product 21", products[0].Name)
-	assert.Equal(t, "Product 25", products[4].Name)
+	assert.Len(t, products.Products, 5)
+	assert.Equal(t, "Product 21", products.Products[0].Name)
+	assert.Equal(t, "Product 25", products.Products[4].Name)
 
 	products, err = productDB.FindAll(4, 10, "asc")
 	assert.Nil(t, err)
-	assert.Empty(t, products)
+	assert.Empty(t, products.Products)
 
 	products, err = productDB.FindAll(0, 0, "")
 	assert.Nil(t, err)
-	assert.Len(t, products, 25)
-	assert.Equal(t, "Product 1", products[0].Name)
-	assert.Equal(t, "Product 25", products[24].Name)
+	assert.Len(t, products.Products, 25)
+	assert.Equal(t, "Product 1", products.Products[0].Name)
+	assert.Equal(t, "Product 25", products.Products[24].Name)
 
 	products, err = productDB.FindAll(1, 25, "order")
 	assert.Nil(t, err)
-	assert.Len(t, products, 25)
-	assert.Equal(t, "Product 1", products[0].Name)
-	assert.Equal(t, "Product 25", products[24].Name)
+	assert.Len(t, products.Products, 25)
+	assert.Equal(t, "Product 1", products.Products[0].Name)
+	assert.Equal(t, "Product 25", products.Products[24].Name)
 }
 
 func TestUpdateProduct(t *testing.T) {
